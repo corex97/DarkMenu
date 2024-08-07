a47ea08ff319cd06c033ed6733964af150e738ae
Corex97 <111160319+corex97@users.noreply.github.com>
2024-08-07 06:42:31 +0200

    Update Menu.lua
    updated- some small changes will come in the afternoon
    !7:00 - 21:00 Local time

--- Menu.lua
+++ Menu.lua
@@ -1,9 +1,24 @@
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Dark Menu V 1", IntroText = "Dark Menu V 1 Loading...", HidePremium = false, SaveConfig = false})
+local Window = OrionLib:MakeWindow({Name = "Dark Menu v1.1", IntroText = "Dark Menu Loading...", HidePremium = false, SaveConfig=true, ConfigFolder="Darkmenu"})
+
+OrionLib:MakeNotification({
+	Name = "Dark Menu",
+	Content = "have Fun!",
+	Time = 10
+})
+
 
 -- Universal Scripts
 local Universal = Window:MakeTab({
 	Name = "Universal"
+})
+
+local Section = Universal:AddSection({
+	Name = "Universal Scripts"
+})
+
+local Section = Universal:AddSection({
+	Name = "Should Work in almost all games!"
 })
 
 Universal:AddButton({
@@ -20,15 +35,37 @@ Universal:AddButton({
 	end
 })
 
+
 Universal:AddButton({
-	Name = "System Broken",
+	Name = "System Broken (Not all Functions Work 100%)",
 	Callback = function()
-		loadstring(game:HttpGet("https://raw.githubusercontent.com/H20CalibreYT/SystemBroken/main/script"))()
+		OrionLib:MakeNotification({Name="Dark Menu",Content="system Broken is a Ragdoll Engine Script BUT some Functions work in other games like fly or Tp",Time=7})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/H20CalibreYT/SystemBroken/main/script"))()
     end
 })
 
+Universal:AddButton({
+	Name = "Freecam",
+	Callback = function()
+		_G.FREECAM_MACRO_KB = {Enum.KeyCode.LeftShift, Enum.KeyCode.P} --change keybind for freecam if you want
+     loadstring(game:HttpGet("https://raw.githubusercontent.com/Melishy/melishy-scripts/main/freecam/freecam.lua", true))()
+     OrionLib:MakeNotification({Name="Free Cam",Content="To activate the Freecam, press Shift + P simultaneously",Image="rbxassetid://4483345998",Time=10});end})
+
+
+Universal:AddButton({
+	Name = "FPS Boost",
+	Callback = function()
+		OrionLib:MakeNotification({Name="FPS Boost",Content="Your Game will lag for a moment.",Time=7})
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FPS-BOOSTER/main/FPSBooster.txt"))()
+    end})
+
+
 -- Hubs 
 local Hubs = Window:MakeTab({
+	Name = "Hubs"
+})
+
+local Section = Hubs:AddSection({
 	Name = "Hubs"
 })
 
@@ -49,18 +86,15 @@ Hubs:AddButton({
 Hubs:AddButton({
 	Name = "Orca X (does not Function 100%)",
 	Callback = function()
-		loadstring(
-  game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/latest.lua")
-)()
-	end
-})
+		loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/latest.lua"))()
+        OrionLib:MakeNotification({Name="Dark Menu",Content="Orca X Started!",Time=10});end})
+
 
 Hubs:AddButton({
 	Name = "Domain X (does not Function 100%)",
 	Callback = function()
         loadstring(game:HttpGet(("https://raw.githubusercontent.com/drakker33/rblx-decaying-winter/main/DecayingWinter.lua"), true))()
-	end
-})
+        OrionLib:MakeNotification({Name="Dark Menu",Content="Domain X Started!",Time=10});end})
 
 -- A Dusty Trip Scripts
 if game.placeId == 16389395869 then
@@ -75,7 +109,6 @@ if game.placeId == 16389395869 then
 			loadstring(game:HttpGet("https://raw.githubusercontent.com/artemy133563/Utilities/main/ADustyTrip",true))()
 		end
 	})
-
 end
 
 -- mic up
@@ -86,13 +119,56 @@ if game.placeId == 6884319169 then
 		Name = "Mic Up"
 	})
 
+    local SettingsSection = mic:AddSection({
+        Name = "Mic Up Scripts"
+    })
+
 	mic:AddButton({
-		Name = "Menu",
+		Name = "Kitty",
 		Callback = function()
 			loadstring(game:HttpGet('https://whimper.xyz/kitty'))()
-		end
-	})
+    end })
 
 end
+
+-- settings
+
+local SettingsTab = Window:MakeTab({
+	Name = "Settings / Credits"
+})
+
+local SettingsSection = SettingsTab:AddSection({
+	Name = "Settings"
+})
+
+SettingsSection:AddButton({
+	Name = "Rejoin Server",
+	Callback = function()
+			
+        OrionLib:MakeNotification({Name="Dark Menu",Content="Some Servers dont Let you rejoin!",Time=4})
+     wait(4)
+    
+        local ts = game:GetService("TeleportService")
+
+local p = game:GetService("Players").LocalPlayer
+
+ts:TeleportToPlaceInstance(game.PlaceId, game.JobId, p)
+  	end    
+})
+
+SettingsSection:AddButton({
+	Name = "Kill Dark Menu",
+	Callback = function()
+        OrionLib:Destroy()
+  	end    
+})
+
+local SettingsSection = SettingsTab:AddSection({
+	Name = "Credits"
+})
+
+SettingsSection:AddLabel("Please note: I did not make any of these scripts")
+SettingsSection:AddLabel("shoutout the creators for their hard work.")
+SettingsSection:AddLabel("Made By corex97 (GitHub)")
 
 OrionLib:Init()
