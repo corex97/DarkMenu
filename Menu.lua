61949e57f660ffb6f5e26bac5c4ff5ce21ccd03f
Corex97 <111160319+corex97@users.noreply.github.com>
2024-08-18 00:17:34 +0200

    Update Menu.lua
    #Update From 08/18/2024
    added:
    
    - Fov Changer
    - Zoom Changer
    - Chat Bypass script
    - Leave Game Button
    - config saves For the fov and zoom changer
    - Home section With Annocements

--- Menu.lua
+++ Menu.lua
@@ -1,12 +1,47 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/corex97/DarkMenu/main/essential3.lua", true))()
+print("​〔Dark Menu〕| have Fun!")
+
 local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Dark Menu v1.2", IntroText = "Dark Menu Loading...", HidePremium = false, SaveConfig=true, ConfigFolder="Darkmenu"})
+local Window = OrionLib:MakeWindow({Name = "Dark Menu v1.3", IntroText = "Dark Menu Loading...", HidePremium = false, SaveConfig=true, ConfigFolder="Darkmenu"})
 
 OrionLib:MakeNotification({
 	Name = "Dark Menu",
-	Content = "have Fun!",
+	Content = "thanks for using my script-hub and have Fun!",
 	Time = 10
 })
 
+local player = game.Players.LocalPlayer
+local playerName = game.Players.LocalPlayer.Name
+-- Home 
+local Home = Window:MakeTab({
+	Name = "Home"
+})
+
+Home:AddParagraph(game.Players.LocalPlayer.Name,"Welcome!")
+Home:AddSection({
+	Name = "last Updated on the: 08/18/2024"
+})
+Home:AddSection({
+	Name = "ADDED:"
+})
+Home:AddSection({
+	Name = "- Fov Changer"
+})
+Home:AddSection({
+	Name = "- Zoom Changer"
+})
+Home:AddSection({
+	Name = "- Chat Bypass script"
+})
+Home:AddSection({
+	Name = "- Leave Game Button"
+})
+Home:AddSection({
+	Name = "- config saves For the fov and zoom changer"
+})
+Home:AddSection({
+	Name = "- other stuff"
+})
 
 -- Universal Scripts
 local Universal = Window:MakeTab({
@@ -35,7 +70,6 @@ Universal:AddButton({
 	end
 })
 
-
 Universal:AddButton({
 	Name = "System Broken (Not all Functions Work 100%)",
 	Callback = function()
@@ -51,16 +85,37 @@ Universal:AddButton({
      loadstring(game:HttpGet("https://raw.githubusercontent.com/Melishy/melishy-scripts/main/freecam/freecam.lua", true))()
      OrionLib:MakeNotification({Name="Free Cam",Content="To activate the Freecam, press Shift + P simultaneously",Image="rbxassetid://4483345998",Time=10});end})
 
-
-Universal:AddButton({
-	Name = "FPS Boost",
+ Universal:AddButton({
+	Name = "FPS Boost (Boosts Your FPS!)",
 	Callback = function()
 		OrionLib:MakeNotification({Name="FPS Boost",Content="Your Game will lag for a moment.",Time=7})
         loadstring(game:HttpGet("https://raw.githubusercontent.com/XNEOFF/FPS-BOOSTER/main/FPSBooster.txt"))()
     end})
 
+Universal:AddSlider({
+		Name = "Fov", Min = 70, Max = 120, Default = game:GetService("Workspace").CurrentCamera.FieldOfView, Color =
+		Color3.fromRGB(92,22,197), Increment = 1, ValueName = "", Callback = function(at) game:GetService("Workspace").CurrentCamera.FieldOfView =
+			at end
+	})
 
--- Hubs 
+	Universal:AddSlider({ Name = "Zoom", Min = game.Players.LocalPlayer.CameraMaxZoomDistance, Max = 1000, Default =
+    game.Players.LocalPlayer.CameraMaxZoomDistance, Color = Color3.fromRGB(92,22,197), Increment = 1, ValueName = "", Callback = function(
+        at) game.Players.LocalPlayer.CameraMaxZoomDistance = at end })
+
+local Section = Universal:AddSection({
+        Name = "ChatBypass "
+    })
+
+    Universal:AddButton({
+        Name = "ChatBypass (Does not work For every word!)",
+        Callback = function()
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/corex97/Modified-scripts/main/ChatBypass.lua"))()
+        end})
+
+        Universal:AddLabel("need to press the button once again if")
+        Universal:AddLabel("it dont want to start fully")
+
+-- Hubs
 local Hubs = Window:MakeTab({
 	Name = "Hubs"
 })
@@ -88,7 +143,6 @@ Hubs:AddButton({
 	Callback = function()
 		loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/richie0866/orca/master/public/latest.lua"))()
         OrionLib:MakeNotification({Name="Dark Menu",Content="Orca X Started!",Time=10});end})
-
 
 Hubs:AddButton({
 	Name = "Domain X (does not Function 100%)",
@@ -139,7 +193,6 @@ infos:AddButton({
 		OrionLib:MakeNotification({Name="Dark Menu",Content="Link is in your clipboard! crtl + v / strg + v To put it in your address bar in your browser",Time=15})
 	end })
 
-
 local Section = infos:AddSection({
 	Name = "Credits"
 })
@@ -163,21 +216,29 @@ SettingsSection:AddButton({
 	Callback = function()
         OrionLib:MakeNotification({Name="Dark Menu",Content="Some Servers dont Let you rejoin! wait 4s",Time=4})
      wait(4)
-    
+
         local ts = game:GetService("TeleportService")
 
 local p = game:GetService("Players").LocalPlayer
 
 ts:TeleportToPlaceInstance(game.PlaceId, game.JobId, p)
-  	end    
+  	end
+})
+
+SettingsSection:AddButton({
+	Name = "Leave Game",
+	Callback = function()
+		game:Shutdown()
+  	end
 })
 
 SettingsSection:AddButton({
 	Name = "Kill Dark Menu",
 	Callback = function()
         OrionLib:Destroy()
-  	end    
+  	end
 })
 
+loadstring(game:HttpGet("https://raw.githubusercontent.com/corex97/DarkMenu/essentials/essential.lua", true))()
 
 OrionLib:Init()
